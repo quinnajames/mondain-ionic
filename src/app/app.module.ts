0aead6baa42ea8091e0c8eb5f5be1efc1c40665f
volucrisscr <quinn@volucris.space>
2017-06-13 21:00:58 -0400

    first version of working list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -8,6 +11,13 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCGOb45VB012KLxfvhr8yHBapEvw4iyEC8",
+  authDomain: "mondain-db.firebaseapp.com",
+  databaseURL: "https://mondain-db.firebaseio.com"
+};
+
 
 @NgModule({
   declarations: [
@@ -18,6 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireModule,  } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+  items: FirebaseListObservable<any[]>;
+
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+      this.items = db.list('/words/ZARZUELA');
   }
 
 }
