f0b4736150b5b945c0350ac6efc2123aa2e61721
Quinn James <quinn@volucris.space>
2017-08-23 12:33:15 -0400

    Word stats transaction now knows when word was previously scheduled (for now, just add 1 day if correct)

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Events } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import firebase from 'firebase';
-
+import moment from 'moment';
 
 @Injectable()
 export class FirebaseService {
@@ -20,78 +20,85 @@ export class FirebaseService {
 
     addRemoteQuizWord(alpha: string, solutions: string[], time, next_scheduled, correct: boolean) {
         let user = this.authProvider.getCurrentUser();
-        time = time.toString();
-        let right_answers = 1;
-        let wrong_answers = 0;
+        var right_answers = 1;
+        var wrong_answers = 0;
         if (!correct) {
             right_answers = 0;
             wrong_answers = 1;
         }
-        let trans_object = null;
+        var word_object = {
+            last_correct: time,
+            next_scheduled: next_scheduled,
+            solutions: solutions,
+            right: right_answers,
+            wrong: wrong_answers
+        };
         if (user) {
             firebase.database().ref('/userProfile').child(user.uid).child(alpha).transaction(function (trans) {
                 console.log(trans);
+                function removeSolutions(obj) {
+                    delete obj.solutions;
+                    return obj;
+                }
                 if (trans) {
-                    var right = trans.right;
-                    var wrong = trans.wrong;
-                    if (trans.right == null) {
-                        right = 0;
+                    if (trans.right) {
+                        word_object.right += trans.right;
                     }
-                    if (trans.wrong == null) {
-                        wrong = 0;
+                    if (trans.wrong) {
+                        word_object.wrong += trans.wrong;
                     }
-                    right = trans.right + right_answers;
-                    wrong = trans.wrong + wrong_answers;
-                    trans_object = {
-                        right: right,
-                        wrong: wrong,
-                        solutions: solutions,
-                        last_correct: time,
-                        next_scheduled: next_scheduled
-                    };
-                    return trans_object;
-                };
-                // TODO:
-                // Restructure the error checking below
-                // (See: https://firebase.google.com/docs/reference/js/firebase.database.Reference#transaction)
-                // There is some repeated stuff here, and I want to be able to get this transaction right
-                // so I can use the same pattern elsewhere in the app
+                    if (trans.next_scheduled) {
+                        if (correct) { // regular moment() parse is ms (x in format); moment.unix() parse is s (X in format)
+                            // the moment() function for unix accepts ints, which is what we're working with
+                            word_object.next_scheduled = parseInt(moment(trans.next_scheduled).add('1', 'days').format('x'), 10); // push forward next scheduled time by 1 day
+                        }
+                        else {
+                            if (trans.last_correct) {
+                                word_object.last_correct = parseInt(trans.last_correct, 10);
+                            }
+                            word_object.next_scheduled = parseInt(moment().add('1', 'minutes').format('x'), 10); // move back to 1 minute after now
+                        }
+                    }
+                }
+                delete word_object.solutions; // don't need this in the transaction if solutions are already present; they won't change
+                console.log("word_object to return: ")
+                console.log(removeSolutions(word_object));
+                return removeSolutions(word_object); // returns TO THE DB (if transaction)
             },
                 function (Error, committed, snapshot) {
-                    if (!committed) {
-                        firebase.database().ref('/userProfile/' + user.uid + '/' + alpha).set({
-                            solutions: solutions,
-                            last_correct: time,
-                            next_scheduled: next_scheduled,
-                            right: right_answers,
-                            wrong: wrong_answers
-                        });
+                    if (Error) {
+                        console.log("Error trying to update word stats");
+                        // return null;
+                    }
+                    else if (!committed) {
+                        firebase.database().ref('/userProfile/' + user.uid + '/' + alpha).set(word_object);
+                        // firebase.database().ref('/userProfile/' + user.uid + '/' + alpha).set({
+                        //     solutions: solutions, // have to add the solutions if creating for first time/using set
+                        //     last_correct: time,
+                        //     next_scheduled: next_scheduled,
+                        //     right: right_answers,
+                        //     wrong: wrong_answers
+                        // });
+                    }
+                    else {
+                        console.log("successfully committed");
                     }
                 }, false);
         }
-        if (!trans_object) {
-            trans_object = {
-                solutions: solutions,
-                last_correct: time,
-                next_scheduled: next_scheduled,
-                right: right_answers,
-                wrong: wrong_answers
-            };
-        }
-        return trans_object;
+        return word_object; // returns TO FUNCTION CALLER to get data back
     }
 
     getPerUserWordStats(word: string) {
-    let user = this.authProvider.getCurrentUser();
-    let stats = {
-        lastCorrect: null,
-        nextScheduled: null
-    }
-    if (user) {
-        firebase.database().ref('/userProfile').child(user.uid).child(word).once('value').then(function (snapshot) {
-            //console.log("**snapshot**:" + snapshot);
-            stats.lastCorrect = snapshot.last_correct;
-            stats.nextScheduled = snapshot.next_scheduled;
+        let user = this.authProvider.getCurrentUser();
+        let stats = {
+            lastCorrect: null,
+            nextScheduled: null
+        }
+        if (user) {
+            firebase.database().ref('/userProfile').child(user.uid).child(word).once('value').then(function (snapshot) {
+                //console.log("**snapshot**:" + snapshot);
+                stats.lastCorrect = snapshot.last_correct;
+                stats.nextScheduled = snapshot.next_scheduled;
             });
             return stats;
         }
@@ -115,11 +122,11 @@ export class FirebaseService {
             back: null
         }
         if (word) {
-            firebase.database().ref('/hooks').child(word).once('value').then(function(snapshot) {
+            firebase.database().ref('/hooks').child(word).once('value').then(function (snapshot) {
                 hooks.front = snapshot.front;
                 hooks.back = snapshot.back;
             });
             return hooks;
-        }  
+        }
     }
 }
