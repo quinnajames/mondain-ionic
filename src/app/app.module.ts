346ad1ccb6972b8a1537afe583031a6f3a57cd29
Quinn James <quinn@volucris.space>
2017-09-12 15:01:52 -0400

    Move firebaseconfig to ignored file: if you clone you'll want to set up your own db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,15 +16,7 @@ import { AuthProvider } from '../providers/auth/auth';
 
 import { LocalQuizService, FirebaseService, AngularFireService } from '../app/shared/shared';
 
-export const firebaseConfig = {
-    apiKey: "AIzaSyCGOb45VB012KLxfvhr8yHBapEvw4iyEC8",
-    authDomain: "mondain-db.firebaseapp.com",
-    databaseURL: "https://mondain-db.firebaseio.com",
-    projectId: "mondain-db",
-    storageBucket: "mondain-db.appspot.com",
-    messagingSenderId: "436243055966"
-};
-
+import { firebaseConfig } from './firebase.config';
 
 @NgModule({
   declarations: [
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCGOb45VB012KLxfvhr8yHBapEvw4iyEC8",
+    authDomain: "mondain-db.firebaseapp.com",
+    databaseURL: "https://mondain-db.firebaseio.com",
+    projectId: "mondain-db",
+    storageBucket: "mondain-db.appspot.com",
+    messagingSenderId: "436243055966"
+};
