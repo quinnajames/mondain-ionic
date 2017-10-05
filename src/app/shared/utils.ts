8005e72b35ac927947d27ae82c65993dbb7cf1d8
volucrisscr <quinn@volucris.space>
2017-10-04 21:47:32 -0400

    Fix failed test: error handle on makeAlphagram()

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -3,29 +3,32 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Utils {
-    LexiconSize : Map<number, number>;
-    constructor() { 
+    LexiconSize: Map<number, number>;
+    constructor() {
         const LexiconSize = new Map([
-            [2,     92], 
-            [3,    878], 
-            [4,   3491],
-            [5,   8654],
-            [6,  16999],
-            [7,  27437],
-            [8,  36327],
-            [9,  38795],
+            [2, 92],
+            [3, 878],
+            [4, 3491],
+            [5, 8654],
+            [6, 16999],
+            [7, 27437],
+            [8, 36327],
+            [9, 38795],
             [10, 36595],
             [11, 27862],
             [12, 20437],
             [13, 14061],
-            [14,  9251],
-            [15,  5846]
+            [14, 9251],
+            [15, 5846]
         ]);
     }
 
     makeAlphagram(word) {
-        word = word.toUpperCase().split("").sort().join("");
-        return word;
+        if (word.search(/[^a-zA-Z]+/) === -1) {
+            word = word.toUpperCase().split("").sort().join("");
+            return word;
+        }
+        return '';
     }
 
 }
