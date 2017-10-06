80e46cbc3c3d889713e2e4e146ccf6c422d0141f
volucrisscr <quinn@volucris.space>
2017-10-05 20:53:32 -0400

    Add several more tests for home page custom list

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -38,6 +38,7 @@ const firebaseConfig = {
 };
 let de: DebugElement;
 let el: HTMLElement;
+let ef: DebugElement;
 
 
 describe('Home: HomePage', () => {
@@ -76,11 +77,12 @@ describe('Home: HomePage', () => {
         homePageInstance = null;
         de = null;
         el = null;
-    })
+    });
+
     it('is created', () => {
         expect(fixture).toBeTruthy();
         expect(homePageInstance).toBeTruthy();
-    })
+    });
 
     it('should create a valid instance of HomePage', () => {
         expect(homePageInstance instanceof HomePage).toBe(true);
@@ -93,16 +95,58 @@ describe('Home: HomePage', () => {
         de = fixture.debugElement.query(By.css('ion-title'));
         el = de.nativeElement;
         expect(el.textContent).toContain('Home');
-    })
+    });
 
     it('renders Search Results header', () => {
 
         fixture.detectChanges();
-        
+
         de = fixture.debugElement.query(By.css('h1'));
         el = de.nativeElement;
         expect(el.textContent).toContain('Search Results');
-    })
+    });
+
+    it('starts with custom study list undefined', () => {
+        expect(homePageInstance.userQuizList).toBeUndefined();
+    });
+
+    it('starts showing the Enter Custom List button', () => {
+
+        fixture.detectChanges();
+
+        de = fixture.debugElement.query(By.css('#enterCustomListButton'));
+        el = de.nativeElement;
+        expect(el.textContent).toContain('Enter Custom List');
+    });
+    it('starts NOT showing the Save Custom List button', () => {
+
+        fixture.detectChanges();
+        de = fixture.debugElement.query(By.css('#saveCustomListButton'));
+        expect(de).toBeNull();
+    });
+    it('calls setEmptyCustomList() when the Enter Custom List button is clicked', async(() => {
+        spyOn(homePageInstance, 'setEmptyCustomList');
+        fixture.detectChanges();
+        de = fixture.debugElement.query(By.css('#enterCustomListButton'));
+        de.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(homePageInstance.setEmptyCustomList).toHaveBeenCalled();
+    }));
+
+    it('stops showing the Enter Custom List button when the button is clicked', async(() => {
+        fixture.detectChanges();
+        de = fixture.debugElement.query(By.css('#enterCustomListButton'));
+        expect(de).not.toBeNull();
+        de.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(homePageInstance.displayCustomList).toBe(true);
+    }));
+
+    it('sets displayCustomList as true when setEmptyCustomList() is called', async(() => {
+        homePageInstance.setEmptyCustomList();
+        fixture.detectChanges();
+        expect(homePageInstance.displayCustomList).toBeTruthy;
+    }));
 
 });
 
