2692097a97ac5a0137c38e17337cdc9e2e1a0a7e
Quinn James <quinn@volucris.space>
2017-10-12 13:01:05 -0400

    Initial implement of dynamically adjusted font sizes

--- src/pages/quiz/components/quiz-question.component.ts
+++ src/pages/quiz/components/quiz-question.component.ts
@@ -2,10 +2,25 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'quiz-question',
-    template: `<ion-item id="quizAnswer" class="text quizQuestion translucent" *ngIf="question">
-                {{ question }}
+    template: `<ion-item id="quizAnswer" class="translucent"
+                *ngIf="question">
+                <span [ngClass]="{'quizQuestion': question.length < 9,
+                'quizNine': question.length >= 9 && question.length < 12,
+                'quizTwelve': question.length >= 12}">
+                {{question}}</span>
                 </ion-item>`
 })
 export class QuizQuestionComponent {
     @Input() question:string;
+    quizClass: string;
+    constructor() {
+        this.quizClass = 'quizQuestion';
+        
+    }
+    addStyle() {
+        if (this.question) {
+            if(this.question.length >= 12) this.quizClass = 'quizTwelve';
+            else if (this.question.length >= 9) this.quizClass = 'quizNine';
+        }
+    }
 }
