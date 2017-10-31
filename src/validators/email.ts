1678a05fbc6b29786242165172226410c61ca6c7
volucrisscr <quinn@volucris.space>
2017-10-31 18:39:03 -0400

    Use W3C email regex

--- src/validators/email.ts
+++ src/validators/email.ts
@@ -3,7 +3,7 @@ import { FormControl } from '@angular/forms';
 export class EmailValidator {
   
   static isValid(control: FormControl){
-    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(control.value);
+    const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(control.value);
     
     if (re){
       return null;
