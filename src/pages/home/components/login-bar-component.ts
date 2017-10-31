148208a8cf41444c12912e0742292720eb7f6a30
volucrisscr <quinn@volucris.space>
2017-10-31 16:20:24 -0400

    add logout button

--- src/pages/home/components/login-bar-component.ts
+++ src/pages/home/components/login-bar-component.ts
@@ -6,13 +6,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <span class="login-item">
         Logged in as: {{userIdent}}</span>
     <span class="login-item"><button ion-button *ngIf="!loggedIn" (click)="refreshLogin()">Login</button></span>
+    <span class="login-item"><button ion-button *ngIf="loggedIn" (click)="logout()">Logout</button></span>
   </ion-item>`
 })
 export class LoginBarComponent {
     @Input() userIdent:string;
     @Input() loggedIn:boolean;
     @Output() clickLogin = new EventEmitter();
+    @Output() clickLogout = new EventEmitter();
     refreshLogin() {
         this.clickLogin.emit();
+    }
+    logout() {
+        this.clickLogout.emit();
     }
 }
