81fccd29a88677af03a972e1dd4e687c1a3de7e3
Quinn James <quinn@volucris.space>
2017-11-05 12:43:54 -0500

    re-delete config file

--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCGOb45VB012KLxfvhr8yHBapEvw4iyEC8",
-    authDomain: "mondain-db.firebaseapp.com",
-    databaseURL: "https://mondain-db.firebaseio.com",
-    projectId: "mondain-db",
-    storageBucket: "mondain-db.appspot.com",
-    messagingSenderId: "436243055966"
-};
